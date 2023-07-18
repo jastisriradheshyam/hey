58782fa9461d6281a0bd4444b46df96edab873aa
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2023-07-18 17:41:34 +0530

    add exec command output stream support
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

--- go/execution/command.go
+++ go/execution/command.go
@@ -3,11 +3,47 @@ package execution
 import (
 	"fmt"
 	"os/exec"
+	"unicode/utf8"
 )
 
 func executeCommand(command string, args ...string) {
 	// Command to execute
 	cmd := exec.Command(command, args...)
+
+	// Used if not record to be stored, directly piping to stream
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+
+	// can be Used to store data before piping to prompt
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	cmd.Start()
+	oneRune := make([]byte, utf8.UTFMax)
+	for {
+		count, err := stdoutPipe.Read(oneRune)
+		if err != nil {
+			break
+		}
+		fmt.Printf("%s", oneRune[:count])
+	}
+
+	oneRuneStderr := make([]byte, utf8.UTFMax)
+	for {
+		count, err := stderrPipe.Read(oneRuneStderr)
+		if err != nil {
+			break
+		}
+		fmt.Printf("%s", oneRuneStderr[:count])
+	}
 
 	// // Create pipes to capture stdout and stderr
 	// stdoutPipe, err := cmd.StdoutPipe()
@@ -41,20 +77,20 @@ func executeCommand(command string, args ...string) {
 	// 	fmt.Println("Error reading stderr:", err)
 	// 	return
 	// }
-	combinedOutput, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	// Wait for the command to finish
-	// err = cmd.Wait()
+	// combinedOutput, err := cmd.CombinedOutput()
 	// if err != nil {
-	// 	fmt.Println("Command finished with error:", err)
+	// 	fmt.Println("Error:", err)
 	// 	return
 	// }
+	// Wait for the command to finish
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println("Command finished with error:", err)
+		return
+	}
 
 	// Print stdout and stderr
-	fmt.Println(string(combinedOutput))
+	// fmt.Println(string(combinedOutput))
 	// fmt.Println("Stdout:", string(stdout))
 	// fmt.Println("Stderr:", string(stderr))
 }
