39791c9bfe49b4f55bca0ed734b6b1378a7be807
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2023-10-01 16:59:16 +0530

    clean up of executeCommand function
    Signed-off-by: Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>

--- go/execution/command.go
+++ go/execution/command.go
@@ -10,11 +10,6 @@ import (
 	"unicode/utf8"
 )
 
-// func test(cmd *exec.Cmd) {
-// 	time.Sleep(4 * time.Second)
-// 	cmd.Process.Signal(syscall.SIGTERM)
-// }
-
 func captureStandardOutOrErrAndShow(stdIOutOrErrPipe io.ReadCloser, stdChan chan bool) {
 	oneRuneStdOutOrErr := make([]byte, utf8.UTFMax)
 	for {
@@ -28,7 +23,6 @@ func captureStandardOutOrErrAndShow(stdIOutOrErrPipe io.ReadCloser, stdChan chan
 }
 
 func executeCommand(command string, envVars map[string]string, args ...string) {
-	// Command to execute
 	var err error
 	stdOutChan := make(chan bool, 1)
 	stdErrChan := make(chan bool, 1)
@@ -40,11 +34,8 @@ func executeCommand(command string, envVars map[string]string, args ...string) {
 	for key, value := range envVars {
 		cmd.Env = append(cmd.Env, key+"="+value)
 	}
-	// Used if not record to be stored, directly piping to stream
-	// cmd.Stdout = os.Stdout
-	// cmd.Stderr = os.Stderr
 
-	// can be Used to store data before piping to prompt
+	// Prepare standard output and error streams
 	stdoutPipe, err := cmd.StdoutPipe()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -56,76 +47,22 @@ func executeCommand(command string, envVars map[string]string, args ...string) {
 		return
 	}
 
+	// Start the sub process
 	err = cmd.Start()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
+	// Handle output in async fashion
 	go captureStandardOutOrErrAndShow(stdoutPipe, stdOutChan)
 	go captureStandardOutOrErrAndShow(stderrPipe, stdErrChan)
-	// oneRuneStdout := make([]byte, utf8.UTFMax)
-	// for {
-	// 	count, err := stdoutPipe.Read(oneRuneStdout)
-	// 	if err != nil {
-	// 		break
-	// 	}
-	// 	fmt.Printf("%s", oneRuneStdout[:count])
-	// }
 
-	// oneRuneStderr := make([]byte, utf8.UTFMax)
-	// for {
-	// 	count, err := stderrPipe.Read(oneRuneStderr)
-	// 	if err != nil {
-	// 		break
-	// 	}
-	// 	fmt.Printf("%s", oneRuneStderr[:count])
-	// }
-
-	// // Create pipes to capture stdout and stderr
-	// stdoutPipe, err := cmd.StdoutPipe()
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-	// stderrPipe, err := cmd.StderrPipe()
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-
-	// // Start the command
-	// err = cmd.Start()
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-
-	// // Read stdout
-	// stdout, err := ioutil.ReadAll(stdoutPipe)
-	// if err != nil {
-	// 	fmt.Println("Error reading stdout:", err)
-	// 	return
-	// }
-
-	// // Read stderr
-	// stderr, err := ioutil.ReadAll(stderrPipe)
-	// if err != nil {
-	// 	fmt.Println("Error reading stderr:", err)
-	// 	return
-	// }
-	// go test(cmd)
-	// combinedOutput, err := cmd.CombinedOutput()
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-	// Wait for the command to finish
+	// Wait for the command to finish or terminate the sub process if main process got terminate signal
 	go func() {
 		<-osSignal
 		cmd.Process.Kill()
 	}()
-	// test(cmd)
 	<-stdOutChan
 	<-stdErrChan
 
@@ -134,10 +71,4 @@ func executeCommand(command string, envVars map[string]string, args ...string) {
 		fmt.Println("Command finished with error:", err)
 		return
 	}
-
-	// fmt.Println("Command finished")
-	// Print stdout and stderr
-	// fmt.Println(string(combinedOutput))
-	// fmt.Println("Stdout:", string(stdout))
-	// fmt.Println("Stderr:", string(stderr))
 }
